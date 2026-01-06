33068ad6c17fb3adbec220e20d3a6b87b675d92f
Vishal Agarwal <vishal.agarwal@zupee.in>
2026-01-06 20:57:24 +0530

    Added code for json path

--- src/test/java/tests/GET/PingTest.java
+++ src/test/java/tests/GET/PingTest.java
@@ -1,24 +1,51 @@
 package tests.GET;
 
 import base.BaseTest;
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 public class PingTest extends BaseTest {
 
     @Test
     public void testPing() {
-//        RestAssured.baseURI = "http://localhost:3000";
+        given().
+        when()
+              .get("/health").
+        then()
+              .log().all()
+              .statusCode(200)
+              .body("ping", equalTo("pong"));
+    }
 
-        given()
-                .when()
-                    .get("/health")
-                .then()
-                    .log().all()
-                    .statusCode(200)
-                    .body("ping", equalTo("pong"));
+    @Test
+    public void testStatusResponseWithJsonPath() {
+        Response response = RestAssured.given().get("/health").andReturn();
+        Assert.assertTrue(response.statusCode() == 200, "Response status code is incorrect");
+
+        JsonPath jsonPath = response.jsonPath();
+        String result = jsonPath.getString("status");
+        System.out.println(result);
+    }
+
+    @Test
+    public void testNotificationResponseWithJsonPath() {
+        Response response = RestAssured.given().get("/users").andReturn();
+        Assert.assertTrue(response.statusCode() == 200, "Response status code is incorrect");
+
+        JsonPath jsonPath = response.jsonPath();
+        /*
+        find {} → Groovy filter expression
+        It scans the array
+        Returns the first matching object
+        Then you navigate inside it
+         */
+        String notifications = jsonPath.getString("find { it.name == 'Naina Agarwal' }.preferences.notifications[0]");
+        System.out.println(notifications);
     }
 }
 
